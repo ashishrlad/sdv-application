ff68a6c68dace657043a6866e9cac471ecad9989
Ashish <ashishrlad@gmail.com>
2025-09-11 16:41:33 +0530

    node exporter added with dashboard of grafanna with dashboard import

--- scripts/monitoring.sh
+++ scripts/monitoring.sh
@@ -54,6 +54,7 @@ kubectl apply -f kubernetes/monitoring/prometheus-service.yaml
 kubectl delete pod -l app=prometheus -n monitoring
 kubectl apply -f kubernetes/cluster-level/pv/grafana-pv.yaml.tmp
 kubectl apply -f kubernetes/monitoring/grafana-pvc.yaml
+kubectl apply -f kubernetes/monitoring/grafana-datasource-configmap.yaml
 kubectl apply -f kubernetes/monitoring/grafana-dashboard-configmap.yaml
 kubectl apply -f kubernetes/monitoring/grafana-deployment.yaml
 kubectl apply -f kubernetes/monitoring/grafana-service.yaml

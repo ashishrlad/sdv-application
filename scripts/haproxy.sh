bb3c8968632714d2c8e767951660191cdc81e5fe
Ashish <ashishrlad@gmail.com>
2025-09-11 11:10:10 +0530

    minio changes

--- scripts/haproxy.sh
+++ scripts/haproxy.sh
@@ -55,13 +55,16 @@ defaults
 frontend http_front
    bind *:80
    bind *:443 ssl crt /etc/haproxy/certs/haproxy.pem
+   bind *:30090 ssl crt /etc/haproxy/certs/haproxy.pem
    stats uri /haproxy?stats
 
-   acl path_minio path_beg /minio
+   acl is_minio dst_port 30090
+   #acl path_minio path_beg /minio
    acl path_grafana path_beg /grafana
    acl path_prometheus path_beg /prometheus
 
-   use_backend minio_back if path_minio
+   #use_backend minio_back if path_minio
+   use_backend minio_back if is_minio
    use_backend grafana_back if path_grafana
    use_backend prometheus_back if path_prometheus
    default_backend http_back
@@ -70,7 +73,8 @@ backend http_back
    server web_app 127.0.0.1:30080 check
 
 backend minio_back
-   server minio_server 127.0.0.1:30090 check
+   #server minio_server 127.0.0.1:9001 check
+   server minio_server 127.0.0.1:9000 check
 
 backend grafana_back
    server grafana_server 127.0.0.1:30092 check
